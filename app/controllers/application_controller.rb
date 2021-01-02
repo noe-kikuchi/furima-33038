0806cccf7951a4ba1579503d1316d47a22e73b44
noe-kikuchi <noenoe888815@gmail.com>
2021-01-02 14:56:43 +0900

    バリテーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_action :configre_permitted_parameters, if: :devise_controller?
+
+  private
+  def configre_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birth_date_id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,presence: true
+  validates :email, uniqueness: true
+  validates :password,length: { minimum: 6 }
+  validates :password, format: { with: /\A[a-z0-9]+\z/i }
+  validates :lastname,presence: true
+  validates :firstname,presence: true
+  validates :lastname_kana,presence: true
+  validates :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :firstname_kana,presence: true
+  validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date_id,presence: true
 end
+
+
+  # validates :email,presence: true
+  # validates :password,presence: true
+  # validates :email, inclusion: { in: ["@"] }
+  # # validates :password, password_confirmation: true
+  # /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i
