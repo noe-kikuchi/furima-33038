dbc776200c9bb31fb0000cfc86a4850d58423b8a
noe-kikuchi <noenoe888815@gmail.com>
2021-01-04 16:18:43 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :state_id, :delivery_fee_id, :prefecture_id, :feescheduled_delivery_id, :price, :image).merge(user_id: curent_user.id)
+  end
 end
