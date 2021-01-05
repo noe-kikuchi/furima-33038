40de29e7bb1b68363e76fd40585d824eeda30904
noe-kikuchi <noenoe888815@gmail.com>
2021-01-06 07:18:08 +0900

    itemモデルのバリテーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :category_id, numericality: { other_than: 1 }
-    validates :state_id, numericality: { other_than: 1 }
-    validates :delivery_fee_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :feescheduled_delivery_id, numericality: { other_than: 1 }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :state_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :feescheduled_delivery_id
+    end
     validates :price, numericality: { greater_than: 300, less_than: 9_999_999 }
   end
 end
