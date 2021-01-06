20a25811490acd747988b1ea8794387d4f958b72
noe-kikuchi <noenoe888815@gmail.com>
2021-01-06 17:02:35 +0900

    show.htmlの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :info, :category_id, :state_id, :delivery_fee_id, :prefecture_id,
-                                 :feescheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :state_id, :delivery_fee_id, :prefecture_id, :feescheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
