bf40c418d9686336b1143d56938b9bc90a7e8b26
noe-kikuchi <noenoe888815@gmail.com>
2021-01-08 17:09:17 +0900

    フォームオブジェクト（モデルの作成）

---
+++ app/models/buy_ship.rb
@@ -0,0 +1,17 @@
+class BuyShip
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+  
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city 
+    validates :address
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+  end
+  
+  def save
+    buy = Buy.create(user_id: user_id, item_id: item_id)
+    Ship.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, buy_id: buy.id)
+  end
+end
